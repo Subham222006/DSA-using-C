7dd0539658191abe8f751ff3ab8251cef59efbb8
Subham222006 <subhamnandi222006@gmail.com>
2025-09-07 01:23:20 +0530

    DSA

---
+++ insert_an_element_at_1st_position_in_linked_list.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node * next;
+};
+typedef struct node node;
+int main()
+{
+  node *p,*start=NULL,*temp,*a,*new;
+  int n;
+  printf("How may numbers you want to store :");
+  scanf("%d",&n);
+  for(int i=0;i<n;i++)
+  {
+    p=(node*)malloc(sizeof(node));
+    p->next=NULL;
+    printf("Enter the data : ");
+    scanf("%d",&p->data);
+    if(start==NULL)
+    {
+        start=p;
+    }
+    else
+    {
+        temp->next=p;
+    }
+    temp=p;
+  }
+  printf("The list of the data is : \n");
+  temp=start;
+  while(temp!=NULL)
+  {
+    printf("%d, ",temp->data);
+    a=temp->next;
+    temp=a;
+  }
+  p=(node*)malloc(sizeof(node));
+  printf("\nEnter the data which you want to insert at 1st position : ");
+  scanf("%d",&p->data);
+  p->next=start;
+  start=p;
+  printf("After insert the data at 1st position the list of the data is : \n");
+  temp=start;
+  while(temp!=NULL)
+  {
+    printf("%d, ",temp->data);
+    a=temp->next;
+    temp=a;
+  }
+}
